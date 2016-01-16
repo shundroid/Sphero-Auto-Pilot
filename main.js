a73443d0e4396ad14961cd1f979343941ac13bf9
shundroid <ra24483@qb3.so-net.ne.jp>
2016-01-16 16:35:15 +0900

    Goalシステムを追加

--- main.js
+++ main.js
@@ -4,29 +4,57 @@ var orb = sphero("COM7"); // 自分の Sphero の ID に置き換える
 var myOrb_color_default = "goldenrod";
 var myOrb_color_collision = "mediumspringgreen";
 
-var myOrb_speed = 125; // 速度
+var myOrb_speed = 255; // 速度
 var myOrb_degree = 0; // 初期角度 (0-359 度表記)
 var collision_limit = 1000; // この回数だけ衝突すると終了
 var collision_num = 0;
 
 var setTimeoutId = -1;
 
-var backTime = 7000;
+var refreshTime = 5000;
+
+var isNextCollision = false;
+
+var startTime;
+
+var keypress = require("keypress");
 
 orb.connect(function() {
-
+  startTime = new Date().getTime();
+    console.log("つながりました");
     orb.color(myOrb_color_default);
     orb.detectCollisions();
-
+orb.getPowerState(function(err, data) {
+  if (err) {
+    console.log("error: ", err);
+  } else {
+    console.log("data:");
+    console.log("  recVer:", data.recVer);
+    console.log("  batteryState:", data.batteryState);
+    console.log("  batteryVoltage:", data.batteryVoltage);
+    console.log("  chargeCount:", data.chargeCount);
+    console.log("  secondsSinceCharge:", data.secondsSinceCharge);
+  }
+});
     // 進行方向の変更
-    function changeDegree() {
-        myOrb_degree = (myOrb_degree+90)%360;
+    function changeDegree(deg) {
+        var _deg = (typeof deg === "undefined" ? 270 : deg);
+        console.log(_deg);
+        myOrb_degree = (myOrb_degree+_deg)%360;
     };
 
     // 衝突時に発生するイベント
     orb.on("collision", function() {
-        console.log("痛い！");
-        changeDegree();
+        if (isNextCollision) {
+          console.log("痛い！");
+          changeDegree();
+          if (setTimeoutId !== -1) {
+            clearTimeout(setTimeoutId);
+          }
+          setTimeoutId = setTimeout(refresh, refreshTime);
+        } else {
+          console.log("スキップします");
+        }
         orb.color(myOrb_color_collision);
         orb.roll(myOrb_speed, myOrb_degree);
         collision_num++;
@@ -36,20 +64,41 @@ orb.connect(function() {
               console.log("end");
           });
         }
-        if (setTimeoutId !== -1) {
-          clearTimeout(setTimeoutId);
-        }
-        setTimeoutId = setTimeout(goBack, backTime);
         setTimeout(function() {
           orb.color(myOrb_color_default);
         }, 250);
+        isNextCollision = !isNextCollision;
     });
     
-    function goBack() {
-      console.log("バックします");
+    function refresh() {
+      console.log("リフレッシュします");
       orb.color(myOrb_color_default);
-      orb.roll(myOrb_speed, (myOrb_degree + 180) % 360);
+      changeDegree();
+      orb.roll(myOrb_speed, myOrb_degree);
+      if (setTimeoutId !== -1) {
+        clearTimeout(setTimeoutId);
+      }
+      setTimeoutId = setTimeout(refresh, refreshTime);
     }
     orb.roll(myOrb_speed, myOrb_degree);
-    setTimeoutId = setTimeout(goBack, backTime);
+    setTimeoutId = setTimeout(refresh, refreshTime);
+    
+    keypress(process.stdin);
+    process.stdin.on("keypress", function(ch, key) {
+      if (key.ctrl && key.name === "c") {
+        process.stdin.pause();
+        process.exit();
+      } else if (key.name === "g") {
+        var gTime = new Date().getTime();
+        console.log("now time: " + gTime)
+        console.log("Goal Time: " + ((gTime - startTime) / 1000) + "秒");
+        orb.disconnect(function() {
+            console.log("end");
+        });
+      }
+    });
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    
+    console.log("start time: " + startTime);
 });
