96cb108119a4915b09f497226e80bbea9b8e43b8
shundroid <ra24483@qb3.so-net.ne.jp>
2016-01-16 14:50:52 +0900

    10秒間ぶつからなかったらバックするように変更 | 色をわかりやすく

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 var sphero = require("sphero");
 var orb = sphero("COM7"); // 自分の Sphero の ID に置き換える
 
-var myOrb_color_default = "lightseagreen";
-var myOrb_color_collision = "goldenrod";
+var myOrb_color_default = "red";
+var myOrb_color_collision = "green";
 
 var myOrb_speed = 255; // 速度
 var myOrb_degree = 0; // 初期角度 (0-359 度表記)
-var collision_limit = 10; // この回数だけ衝突すると終了
+var collision_limit = 100; // この回数だけ衝突すると終了
 var collision_num = 0;
 
+var setTimeoutId = -1;
 
+var backTime = 10000;
 
 orb.connect(function() {
 
@@ -34,10 +36,20 @@ orb.connect(function() {
               console.log("end");
           });
         }
+        if (setTimeoutId !== -1) {
+          clearTimeout(setTimeoutId);
+        }
+        setTimeoutId = setTimeout(goBack, backTime);
         setTimeout(function() {
-            orb.color(myOrb_color_default);
-        }, 3000);
+          orb.color(myOrb_color_default);
+        }, 250);
     });
-
+    
+    function goBack() {
+      console.log("バックします");
+      orb.color(myOrb_color_default);
+      orb.roll(myOrb_speed, (myOrb_degree + 180) % 360);
+    }
     orb.roll(myOrb_speed, myOrb_degree);
+    setTimeoutId = setTimeout(goBack, backTime);
 });
